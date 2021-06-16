c1824806a7e187e58bf6ca551ffa0c49b44e7e33
Holovatiuk Victor <Stethem696@gmail.com>
2021-06-16 23:06:20 +0300

    половина лекции

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+// import React, { Component } from 'react';
+import Counter from "./components/Counter/Counter";
 
-const App = console.log("hallo");
+const App = () => (
+  <>
+    <h1>Состояние компонента</h1>
+
+    <Counter initialValue={666} />
+  </>
+);
 
 export default App;
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Counter.css";
+
+class Counter extends React.Component {
+  static defaultProps = { initialValue: 0 };
+
+  state = { value: this.props.initialValue };
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      value: prevState.value + 1,
+    }));
+    console.log("Кликнули увеличить");
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({ value: prevState.value - 1 }));
+    console.log("Кликнули уменшить");
+  };
+  render() {
+    return (
+      <div className="Counter">
+        <span className="Counter__value">{this.state.value}</span>
+
+        <div className="Counter__controls">
+          <button type="button" onClick={this.handleIncrement}>
+            Увеличить на 1
+          </button>
+          <button type="button" onClick={this.handleDecrement}>
+            Уменьшить на 1
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
